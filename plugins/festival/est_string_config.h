7d2d254970272eb179e2215ac1cfeb5745b94679
Gary Cramblitt <garycramblitt@comcast.net>
2005-02-17 23:52:15 +0000

    Delete festival/ and festivalcs/ directories.  They are not built by default, and AFAIK, they never worked.  They are misleading packagers into thinking there is a dependency on libfestival and libspeechtools.
    svn path=/trunk/kdeaccessibility/kttsd/; revision=390435

--- plugins/festival/est_string_config.h
+++
@@ -1,8 +0,0 @@
-// The purpose of this file is to fix a conflict between KDE and Festival.
-// Both use the define HAVE_CONFIG.  The files EST_String.h and EST_Chunk.h
-// both use define HAVE_CONFIG, and if this symbol is defined, want to include
-// a non-existent file called est_string_config.h.  This file satisfies that
-// requirement.
-// 
-#    define HAVE_WALLOC_H (1)
-
