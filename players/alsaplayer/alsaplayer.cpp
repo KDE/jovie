e156f135565d76996a694e2bf7973e51581bd4f3
Gary Cramblitt <garycramblitt@comcast.net>
2005-07-23 02:19:05 +0000

    autoconf manual says to do it this way
    svn path=/trunk/KDE/kdeaccessibility/kttsd/; revision=437790

--- players/alsaplayer/alsaplayer.cpp
+++ players/alsaplayer/alsaplayer.cpp
@@ -26,8 +26,17 @@
 
 // #include <sys/wait.h>
 // System includes.
-#include <sys/time.h>
-#include <time.h>
+#include <config.h>
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 
 // Qt includes.
 #include <qdir.h>
