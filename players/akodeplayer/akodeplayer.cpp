b2be8167e47952b7213f00f6b129bbef63841efb
Gary Cramblitt <garycramblitt@comcast.net>
2006-01-18 00:01:23 +0000

    SVN_SILENT cleanup
    svn path=/branches/KDE/3.5/kdeaccessibility/kttsd/; revision=499514

--- players/akodeplayer/akodeplayer.cpp
+++ players/akodeplayer/akodeplayer.cpp
@@ -68,7 +68,6 @@ void aKodePlayer::startPlay(const QString &file)
         }
     }
 
-    // if (m_player->load(file.local8Bit().data()))
     if (m_player->load(QFile::encodeName(file)))
         m_player->play();
 
