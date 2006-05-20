c14ab719a080d3d1d484eb7425fc6268286c1a3d
Laurent Montel <montel@kde.org>
2006-05-20 11:07:05 +0000

    Move check into config-kttsd.h
    svn path=/trunk/KDE/kdeaccessibility/kttsd/; revision=542794

---
+++ config-kttsd.h.cmake
@@ -0,0 +1,9 @@
+/* Define to 1 if you have the <alsa/asoundlib.h> header file. */
+#cmakedefine HAVE_ALSA_ASOUNDLIB_H 1
+
+/* define if you have aKodelib installed */
+#cmakedefine HAVE_AKODE 1
+
+/* Define if you have libasound.so.2 (required for ALSA 0.9.x/1.x support) */
+#define HAVE_LIBASOUND2 1
+
