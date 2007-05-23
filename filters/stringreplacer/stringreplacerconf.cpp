3435a9c124f27133afeddfa4167e7fbf6868d66b
Laurent Montel <montel@kde.org>
2007-05-23 16:34:52 +0000

    Use qobject_cast fix crash when we load KRegExpEditorInterface
    svn path=/trunk/KDE/kdeaccessibility/kttsd/; revision=667691

--- filters/stringreplacer/stringreplacerconf.cpp
+++ filters/stringreplacer/stringreplacerconf.cpp
@@ -673,7 +673,7 @@ void StringReplacerConf::slotMatchButton_clicked()
     {
         // kdeutils was installed, so the dialog was found.  Fetch the editor interface.
 
-        KRegExpEditorInterface *reEditor = dynamic_cast<KRegExpEditorInterface*>( editorDialog );
+        KRegExpEditorInterface *reEditor = qobject_cast<KRegExpEditorInterface*>( editorDialog );
         Q_ASSERT( reEditor ); // This should not fail!// now use the editor.
         reEditor->setRegExp( m_editWidget->matchLineEdit->text() );
         int dlgResult = editorDialog->exec();
