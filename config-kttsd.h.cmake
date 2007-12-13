86b00b1bcc376793bfb25889a7b97d5320271c73
Allen Winter <winter@kde.org>
2007-12-13 16:47:27 +0000

    nothing in the code relies on HAVE_LIBASOUND2 anymore.
    svn path=/trunk/KDE/kdeaccessibility/kttsd/; revision=748112

--- config-kttsd.h.cmake
+++ config-kttsd.h.cmake
@@ -1,6 +1,3 @@
-/* Define if you have libasound.so.2 (required for ALSA 0.9.x/1.x support) */
-#define HAVE_LIBASOUND2 1
-
 /* Define to 1 if you have the <sys/time.h> header file. */
 #cmakedefine HAVE_SYS_TIME_H 1
 
