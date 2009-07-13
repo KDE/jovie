a5bb8081934ea3233202e3abebd137b070cc1e8f
Laurent Montel <montel@kde.org>
2009-07-13 11:46:07 +0000

    not necessary
    svn path=/trunk/KDE/kdeaccessibility/kttsd/; revision=995779

--- Messages.sh
+++ Messages.sh
@@ -2,6 +2,5 @@
 
 $EXTRACTRC */*.rc */*/*.rc >> rc.cpp || exit 11
 $EXTRACTRC */*.ui */*/*.ui >> rc.cpp || exit 12
-$EXTRACTRC --tag=name --context=FestivalVoiceName plugins/festivalint/voices >> rc.cpp
 $XGETTEXT rc.cpp */*.cpp */*.h */*/*.cpp */*/*.h -o $podir/kttsd.pot
 rm -f rc.cpp
