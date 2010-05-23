ed52179f3ccb868487c140279ed59f4890a4483c
Jeremy Paul Whiting <jpwhiting@kde.org>
2010-05-23 18:43:15 +0000

    rename the .pot file
    svn path=/trunk/KDE/kdeaccessibility/jovie/; revision=1129786

--- Messages.sh
+++ Messages.sh
@@ -2,5 +2,5 @@
 
 $EXTRACTRC */*.rc */*/*.rc >> rc.cpp || exit 11
 $EXTRACTRC */*.ui */*/*.ui >> rc.cpp || exit 12
-$XGETTEXT rc.cpp */*.cpp */*.h */*/*.cpp */*/*.h -o $podir/kitty.pot
+$XGETTEXT rc.cpp */*.cpp */*.h */*/*.cpp */*/*.h -o $podir/jovie.pot
 rm -f rc.cpp
