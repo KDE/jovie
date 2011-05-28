c7c7f25f88069adb8b479a8d41e7432acf56f781
Jeremy Paul Whiting <jpwhiting@kde.org>
2011-05-28 15:42:52 +0000

    remove old .cmake file, we no longer need time.h
    svn path=/trunk/KDE/kdeaccessibility/jovie/; revision=1233915

--- KttsdConfigureChecks.cmake
+++
@@ -1,5 +0,0 @@
-include(CheckIncludeFile)
-include(CheckIncludeFiles)
-
-check_include_files(sys/time.h HAVE_SYS_TIME_H)
-check_include_files("sys/time.h;time.h" TIME_WITH_SYS_TIME)
--- config-jovie.h.cmake
+++ config-jovie.h.cmake
@@ -1,10 +1,2 @@
-/* Define to 1 if you have the <sys/time.h> header file. */
-#cmakedefine HAVE_SYS_TIME_H 1
-
-/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
-#cmakedefine TIME_WITH_SYS_TIME 1
-
 #cmakedefine SPEECHD_FOUND ${SPEECHD_FOUND}
-
-#cmakedefine OPENTTS_FOUND ${OPENTTS_FOUND}
 
